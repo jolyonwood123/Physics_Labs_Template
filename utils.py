24e44803ebfe62fffc472500287e6b441b5e71b0
Jolyon Wood <jolyonwood123@gmail.com>
2026-06-04 19:08:01 +0100

    fixed and refactored BaseLabs class and implemented basic plot generation functionality

--- utils.py
+++ utils.py
@@ -9,26 +9,44 @@ import time
 load_dotenv()
 raw_configs = os.getenv("DATA_CONFIGS", '{}')
 data_configs = json.loads(raw_configs)
+plot_dir = str(os.getenv("PLOTSDIR"))
+print(plot_dir)
+filenames = []
 
-
-start = time.time()
 def get_data(data_configs:dict=data_configs):
     DATA_DIR = Path(__file__).resolve().parent / 'data'
     data_store = {}
-    # csv_paths = list(DATA_DIR.glob('*.csv'))
     
     for filename, row_remove in data_configs.items():
         path = DATA_DIR / f"{filename}.csv"
         if path.exists():
             data_store[filename] = pd.read_csv(path, skiprows=row_remove)
+            filenames.append(filename)
     return data_store
         
 class BaseLab:
-    def __init__(self, df: pd.DataFrame):
-        self._df = df
+    
+    def __init__(self, filename:str, plot_dir=plot_dir):
+        self._df = data_store[filename]
+        self.name = filename
+        self.projectdir = os.path.dirname(os.path.abspath(__file__))
+        self.plot_dir = self.projectdir + plot_dir
+
+
         
+        print(self.projectdir)
+        if not os.path.exists(self.plot_dir):
+            os.makedirs(self.plot_dir)
         
 
+
+    def timeit(self, func, *args, **kwargs ):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print(f"Operation took {end - start:.4f} seconds.")
+        return result
+           
     def __getattr__(self, name):
         return getattr(self._df, name)
     
@@ -43,28 +61,63 @@ class BaseLab:
         self._df[column] = data * conversion_factor
         return self._df[column]
     
-    def graph(self, xaxis: str, yaxis: str, figsize:tuple=(10,10)):
+    def create_2dgraph(self, xaxis: str, yaxis: str, figsize:tuple=(10,10)):
         df = self._df
-        
+        self.last_xaxis = xaxis
+        self.last_yaxis = yaxis
 
         plt.figure(figsize=figsize)
-        plt
         plt.plot(df[xaxis], df[yaxis], label="", color='red')
         plt.title(f"{yaxis.upper().replace("_", " ")} vs {xaxis.upper().replace("_", " ")}")
         plt.xlabel(xaxis.replace("_", " "))
         plt.ylabel(yaxis.replace("_", " "))
         plt.grid(True)          
         plt.legend()
+        return plt, xaxis, yaxis
+        
+        
+    def display_2dgraph(self):
         plt.tight_layout()
         plt.show()
+    def save_plot(self, plot_name:str="untitled"):
+
+        class_name = type(self).__name__
+        if plot_name == "untitled":
+            plot_name = f"{self.name}_{self.last_xaxis}_vs_{self.last_yaxis}"
+
+        save_path = os.path.join(self.projectdir, self.plot_dir, plot_name)
+        print(save_path)
+        counter = 0
+        while os.path.exists(f"{save_path}.png"):
+            counter += 1
+            save_path = f"{save_path}_V{counter}"
+            
+
+        plt.savefig(save_path, dpi=300)
+        
+        plt.close()
+        print(f"Graph saved to: {self.projectdir}{self.plot_dir}as {plot_name}.png")
+
     
+        
+        
+   
+        
+        
+
 
     
 data_store = get_data()
-test = BaseLab(data_store['kinematics'])
+test = BaseLab('kinematics')
 
 # print(test.converter('velocity_mps', 1000))
-print(test.graph('distance_m', 'velocity_mps'))
+print(test.timeit(test.create_2dgraph, 'distance_m', 'velocity_mps'))
+test.save_plot()
+
+
+
+
+
 
 
 
